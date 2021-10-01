87b1b72aaeeb5d3ea70cd8949c93f4d057b09470
giliover <giliover@hotmail.com>
2021-10-01 20:53:17 -0300

    chore(shared_server): add http server and routes into shared

---
+++ server/src/shared/infra/http/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import productsRouter from '@modules/products/infra/http/routes/products.routes';
+
+const routes = Router();
+
+routes.use('/sessions', sessionsRouter);
+routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
+routes.use('/products', productsRouter);
+
+export default routes;
